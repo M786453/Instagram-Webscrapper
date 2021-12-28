a0a3e0148852e5c161283a5666dc079abc59d8d5
DeV_MaS <ahteshamsarwar333@gmail.com>
2021-12-28 12:46:48 +0500

    complete project

--- main.py
+++ main.py
@@ -8,69 +8,65 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 
-wb = openpyxl.load_workbook("E:/InstaScrapProject/followers.xlsx")
+
+wb = openpyxl.load_workbook("followers.xlsx")
 
 sheets = wb.sheetnames
 
-followers = []
+sheet_followers_dict = {}
 
-followers_col = wb['pesaschile']['A']
+for sheet in sheets:
+    followers = []
 
-for f in followers_col:
-    followers.append(f.value)
+    followers_col = wb[sheet]['A']
 
-driver = webdriver.Chrome("C:/Users/Ahtesham Sarwar/Downloads/chromedriver_win32/chromedriver.exe")
+    for follower in followers_col:
+        if str(type(follower.value)) != "<class 'NoneType'>":
+            followers.append(follower.value)
 
-# driver.get("https://www.instagram.com/")
+    sheet_followers_dict.update({sheet: followers})
 
-
-
-
-# username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
-# password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
 #
-# username.clear()
-# password.clear()
-#
-# username.send_keys("USERNAME-HERE")
-#
-# password.send_keys("PASSWORD-HERE")
-#
-# logIn = WebDriverWait(driver, 10).until(
-#     EC.element_to_be_clickable((By.XPATH, '//div[contains(text(),"Log In")]'))).click()
-#
-# not_now_btn = WebDriverWait(driver, 10).until(
-#     EC.element_to_be_clickable((By.XPATH, '//button[contains(text(),"Not Now")]'))).click()
-#
-# not_now_btn2 = WebDriverWait(driver, 10).until(
-#     EC.element_to_be_clickable((By.XPATH, '//button[contains(text(),"Not Now")]'))).click()
+# options = Options()
+# options.headless = True     # with this option, we will scrap in background
+# driver = webdriver.Chrome("./chromedriver_win32/chromedriver.exe", options=options)
+
+driver = webdriver.Chrome("./chromedriver_win32/chromedriver.exe")
 
 driver.get("https://www.instagram.com/home/")
 
-for s in range(0, len(followers)):
+for sheet in sheet_followers_dict:
 
-    search = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='Search']")))
+    followers = sheet_followers_dict[sheet]
 
-    search.clear()
+    if len(followers) > 0:
 
-    search.send_keys(followers[s])
+        for f in range(0, len(followers)):
 
-    time.sleep(5)
+            search = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='Search']")))
 
-    search.send_keys(Keys.ENTER)
+            search.clear()
 
-    time.sleep(5)
+            search.send_keys(followers[f])
 
-    search.send_keys(Keys.ENTER)
+            time.sleep(5)
 
-    try:
-        time.sleep(5)
-        driver.find_element_by_class_name("rkEop")
-        wb['pesaschile']["B" + str(s+1)] = "Account is Private"  # writing account status in excel file
-        print(followers[s] + "'s Account is Private")
-    except NoSuchElementException:
-        wb['pesaschile']["B" + str(s+1)] = "Account is Public"
-        print(followers[s] + "'s Account is Public")
+            search.send_keys(Keys.ENTER)
+
+            time.sleep(5)
+
+            search.send_keys(Keys.ENTER)
+
+            try:
+                time.sleep(5)
+                driver.find_element_by_class_name("rkEop")
+                wb[sheet]["B" + str(f + 1)] = "Account is Private"  # writing account status in excel file
+                print(followers[f] + "'s Account is Private")
+            except NoSuchElementException:
+                wb[sheet]["B" + str(f + 1)] = "Account is Public"
+                print(followers[f] + "'s Account is Public")
 
 wb.save("New.xlsx")
